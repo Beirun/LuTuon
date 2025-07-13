c03948cb1f7408265e084211cef61825eefb90d9
Beirun <orillobernardjay@gmail.com>
2025-07-14 03:13:10 +0800

    Update ButtonSceneLoader script

--- Assets/Scripts/ButtonSceneLoader.cs
+++ Assets/Scripts/ButtonSceneLoader.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class ButtonSceneLoader : MonoBehaviour
 {
     [SerializeField] public string sceneName; // Set this in the Inspector
+    [SerializeField] public bool withTransition = false;
     private Button targetButton;
 
     void Start()
@@ -16,7 +17,9 @@ public class ButtonSceneLoader : MonoBehaviour
         {
             targetButton.onClick.AddListener(() =>
             {
-                if (SceneLoader.Instance != null)
+                if (withTransition && SceneLoader.Instance != null)
+                    SceneLoader.Instance.LoadSceneWithTransition(sceneName);
+                else if (SceneLoader.Instance != null)
                     SceneLoader.Instance.LoadScene(sceneName);
                 else
                     Debug.LogWarning("SceneController instance not found.");
