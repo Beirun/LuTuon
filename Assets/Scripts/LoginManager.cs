e176169d9f83f609e71184837411f80a6121f38b
Beirun <orillobernardjay@gmail.com>
2025-09-25 15:14:28 +0800

    Update LoginManager script

--- Assets/Scripts/LoginManager.cs
+++ Assets/Scripts/LoginManager.cs
@@ -1,16 +1,44 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public TMP_InputField emailInput;
+    public TMP_InputField passwordInput;
+    public Button loginButton;
+
+    public AuthManager authManager;
+
+    [SerializeField] DialogManager dialogManager;
+    [SerializeField] LoginButtonController loginButtonController; // drag in Inspector
+
+    private void Start()
     {
-        
+        loginButton.onClick.AddListener(OnLoginClicked);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnLoginClicked()
     {
-        
+        string email = emailInput.text;
+        string password = passwordInput.text;
+
+        if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+        {
+            Debug.LogWarning("Email and Password cannot be empty");
+            return;
+        }
+
+        authManager.Login(email, password, (success, error) =>
+        {
+            if (success)
+            {
+                Debug.Log("Login successful!");
+                dialogManager.CloseDialog("LoginDialog");
+                loginButtonController.Refresh();
+            }
+            else
+                Debug.LogError("Login failed: " + error);
+        });
     }
 }
