8d035453348b8bc0b68784180e8f3a95892395dc
Beirun <orillobernardjay@gmail.com>
2025-09-25 15:14:38 +0800

    Add ReturnToMenuLoader script

---
+++ Assets/Scripts/ReturnToMenuLoader.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ReturnToMenuLoader : MonoBehaviour
+{
+    public void ReturnToMainMenu()
+    {
+        LoadSceneAndShowMenu("MainMenuScene",
+            () => MenuCanvasLoader.Instance.ShowMainMenu());
+    }
+
+    public void ReturnToTutorialMenu()
+    {
+        LoadSceneAndShowMenu("MainMenuScene",
+            () => MenuCanvasLoader.Instance.ShowTutorialMenu());
+    }
+
+    public void ReturnToStandardMenu()
+    {
+        LoadSceneAndShowMenu("MainMenuScene",
+            () => MenuCanvasLoader.Instance.ShowStandardMenu());
+    }
+
+    void LoadSceneAndShowMenu(string sceneName, System.Action menuAction)
+    {
+        if (SceneLoader.Instance == null)
+        {
+            Debug.LogError("ReturnToMenuLoader: SceneLoader not found.");
+            return;
+        }
+
+        void Handler(Scene s, LoadSceneMode m)
+        {
+            SceneManager.sceneLoaded -= Handler;
+
+            if (MenuCanvasLoader.Instance == null)
+            {
+                Debug.LogError("MenuCanvasLoader not found after scene load.");
+                return;
+            }
+
+            MenuCanvasLoader.Instance.StartCoroutine(WaitForCanvases(menuAction));
+        }
+
+        SceneManager.sceneLoaded += Handler;
+        SceneLoader.Instance.LoadSceneWithTransition(sceneName);
+    }
+
+    IEnumerator WaitForCanvases(System.Action callback)
+    {
+        // wait until MenuCanvasLoader assigned its Canvas components
+        while (MenuCanvasLoader.Instance == null)
+        {
+            yield return null;
+        }
+        callback();
+    }
+}
