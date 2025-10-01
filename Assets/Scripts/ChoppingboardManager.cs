b5a04a8ad30e2ff78e9e2a16a347503e6f7af87e
Beirun <orillobernardjay@gmail.com>
2025-10-01 22:22:08 +0800

    Add ChoppingboardManager.cs

---
+++ Assets/Scripts/ChoppingboardManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ChoppingboardManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [HideInInspector]
+    public bool isOccupied = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
