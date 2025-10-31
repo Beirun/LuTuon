71695cb96fbe233d315e91682367d678936eed05
Beirun <orillobernardjay@gmail.com>
2025-10-31 23:16:55 +0800

    Add ChoppedGingerController.cs

---
+++ Assets/Scripts/ChoppedGingerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+public class ChoppedGingerController : DragController
+{
+
+
+    public IEnumerator AnimatePlacement(Vector3 targetPos, Quaternion targetRot, float duration)
+    {
+        Vector3 fromPos = transform.position;
+        Quaternion fromRot = transform.rotation;
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+            t = t * t * (3f - 2f * t);
+
+            Vector3 pos = Vector3.Lerp(fromPos, targetPos, t);
+
+
+            transform.position = pos;
+            transform.rotation = Quaternion.Slerp(fromRot, targetRot, t);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
