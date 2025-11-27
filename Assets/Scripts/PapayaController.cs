e9a0d9285553b24a255a1ac4e6ef9c3a3d476a31
Beirun <orillobernardjay@gmail.com>
2025-11-27 11:15:45 +0800

    Update PapayaController

--- Assets/Scripts/PapayaController.cs
+++ Assets/Scripts/PapayaController.cs
@@ -71,11 +71,13 @@ public class PapayaController : DragController
             if(water.transform.position.y < 1f) p = highlighted.transform.position + new Vector3(0f, 0.2f, 0f);
             StartCoroutine(AnimatePlacement(p, transform.rotation, 0.5f));
         }
+        else StartCoroutine(ReturnToStart());
         ClearHighlight();
     }
 
     public IEnumerator AnimatePlacement(Vector3 targetPos, Quaternion targetRot, float duration, bool isDragging = true)
     {
+        isPerforming = true;
         Vector3 a = transform.position;
         Quaternion b = transform.rotation;
         float t0 = 0f;
@@ -95,6 +97,8 @@ public class PapayaController : DragController
             EnablePhysicsOnChildren(transform);
             isFinished = true;
         }
+        this.isDragging = false;
+        isPerforming = false;
     }
 
     void EnablePhysicsOnChildren(Transform p)
