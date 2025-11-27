b0c340e2de01865db1be7218672106884a9de9ad
Beirun <orillobernardjay@gmail.com>
2025-11-27 11:15:49 +0800

    Add ChickenSoyManager

---
+++ Assets/Scripts/ChickenSoyManager.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChickenSoyManager : MonoBehaviour
+{
+    public GameObject chicken;
+    public GameObject water;
+
+    List<Material> mats = new List<Material>();
+    Color[] startColors;
+    Color target = new Color(0.6132076f, 0.4878468f, 0.4830455f);
+    bool started;
+
+    void Start()
+    {
+        if (!chicken) return;
+        Renderer[] r = chicken.GetComponentsInChildren<Renderer>(true);
+        foreach (var e in r)
+        {
+            // Use sharedMaterial to avoid material instancing explosion
+            Material m = e.material;
+            mats.Add(m);
+        }
+
+        startColors = new Color[mats.Count];
+        for (int i = 0; i < mats.Count; i++)
+        {
+            Material m = mats[i];
+            if (m.HasProperty("_BaseColor"))
+                startColors[i] = m.GetColor("_BaseColor");
+            else if (m.HasProperty("_Color"))
+                startColors[i] = m.GetColor("_Color");
+            else
+                startColors[i] = Color.white;
+        }
+    }
+
+    void Update()
+    {
+        if (!started && water && water.activeInHierarchy)
+        {
+            started = true;
+            StartCoroutine(DoTransition());
+        }
+    }
+
+    IEnumerator DoTransition()
+    {
+        yield return new WaitForSeconds(5f);
+
+        float d = 5f;
+        float t = 0f;
+
+        while (t < d)
+        {
+            float k = t / d;
+            for (int i = 0; i < mats.Count; i++)
+            {
+                Material m = mats[i];
+                Color c = Color.Lerp(startColors[i], target, k);
+
+                if (m.HasProperty("_BaseColor"))
+                    m.SetColor("_BaseColor", c);
+                else if (m.HasProperty("_Color"))
+                    m.SetColor("_Color", c);
+            }
+
+            t += Time.deltaTime;
+            yield return null;
+        }
+
+        for (int i = 0; i < mats.Count; i++)
+        {
+            Material m = mats[i];
+            if (m.HasProperty("_BaseColor"))
+                m.SetColor("_BaseColor", target);
+            else if (m.HasProperty("_Color"))
+                m.SetColor("_Color", target);
+        }
+    }
+}
