27c94c436df5842661103139cca21f793f1a0230
Beirun <orillobernardjay@gmail.com>
2025-12-20 01:25:40 +0800

    Update OnionManager

--- Assets/Scripts/Onion/OnionManager.cs
+++ Assets/Scripts/Onion/OnionManager.cs
@@ -23,6 +23,10 @@ public class OnionManager : MonoBehaviour
 
     [Header("Diced Onions Controller")]
     public DicedOnionsController dicedOnionsController;
+
+    [Header("Transform")]
+    public Quaternion rotation = Quaternion.Euler(0f, 0f, 0f);
+    public Vector3 position = new Vector3(0f, -.25f, -0.25f);
     void Start()
     {
 
@@ -85,8 +89,10 @@ public class OnionManager : MonoBehaviour
                 dicedOnion_4.SetActive(true);
                 onionController.enabled = false;
                 knifeController.cutsMade = 0;
-                dicedOnionsController.startPos = new Vector3(onionController.startPos.x, onionController.startPos.y - 0.25f, onionController.startPos.z - 0.25f);
-                StartCoroutine(dicedOnionsController.AnimatePlacement(dicedOnionsController.startPos, Quaternion.Euler(0f, 0f, 0f), 0.75f, false));
+                dicedOnionsController.startPos = onionController.startPos + position;
+                // dicedOnionsController.startPos = new Vector3(onionController.startPos.x, onionController.startPos.y - 0.25f, onionController.startPos.z - 0.25f);
+                dicedOnionsController.startRot = rotation;
+                StartCoroutine(dicedOnionsController.AnimatePlacement(dicedOnionsController.startPos, rotation, 0.75f, false));
                 onionController.isPlaced = false;
             }
         }
